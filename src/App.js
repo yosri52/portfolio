0042cac5766549831040dc4a67054eae84f4763c
Yosri Ben Hsan <dingogrand@gmail.com>
2022-09-11 18:07:16 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Navbar from './compoments/navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
 
 function App() {
   return (
     <div>
-  
-      <Navbar/>
+      <BrowserRouter>
+        <h1> hello </h1>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/skills" component={Skills} />
+       
+      </BrowserRouter>
     </div>
   )
 }
-export default App ;
+export default App;
