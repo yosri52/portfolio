64aefe89fd735bd226ae7b1a1558dcf8521165f2
Yosri Ben Hsan <dingogrand@gmail.com>
2022-09-18 17:12:56 +0200

    add skill details page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route,Switch} from 'react-router-dom'
 import Navbar from './components/navbar'
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import SkillDetails from './pages/SkillDetails';
 import Skills from './pages/Skills';
 
 function App() {
@@ -12,10 +13,13 @@ function App() {
         <h1> hello </h1>
         <Navbar />
         <div className='container mt-5'>
+        <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Projects} />
-        <Route path="/skills" component={Skills} />
+        <Route exact path="/skills" component={Skills} />
+        <Route path="/skills/:id" component={SkillDetails} />
+        </Switch>
        </div>
       </BrowserRouter>
     </div>
